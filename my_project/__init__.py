0df80c2da75baea8cc77558803ccc2c3e224f9ff
jenkins-nicholas <jenkins.nicholas@bcg.com>
2021-11-05 20:04:07 -0400

    update version

--- my_project/__init__.py
+++ my_project/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Copyright © 2021 by Nick Jenkins. All rights reserved
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
@@ -25,7 +26,8 @@ from my_project import utils  # noqa: F401
 # Git tagging is required in addition to these changes
 # See CONTRIBUTING.md for more info
 
-__version__ = "0.1.3"  # Minor bug fixes and instructions improvements
+__version__ = "0.1.4"  # Add docker resources and improve nox and poetry pairing
+# __version__ = "0.1.3"  # Minor bug fixes and instructions improvements
 # "0.1.2"  # Update docs process
 # "0.1.1"  # Rationalizing files
 # "0.1.0" # Initial commit
