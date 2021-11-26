cdd8fe7e5a0eeaea1db8e62ba3e1a9263b629a88
Nick Jenkins <nick_jenkins@outlook.com>
2021-11-26 17:20:40 -0500

    Add a lagged example

---
+++ schemas/queries/window_lag.sql
@@ -0,0 +1,18 @@
+-- Example of window lag function.
+-- LAG can be used to offset rows.
+-- LEAD similar just other direction; kinda useless cause we can use negatives!
+-- Often the order by will match to aid inspection that it's working
+-- The Partition in this case is used to assist the offset by applying
+-- Fare difference according to whether passenger survived or not
+SELECT
+    name,
+    age,
+    survived,
+    fare,
+    LAG(fare, 1) OVER (
+        PARTITION BY survived
+        ORDER BY age, name
+    ) AS fare_change
+FROM titanic
+WHERE age >= 18
+ORDER BY age, name
