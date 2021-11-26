8fa83c7d6c14a24c36a60d1c98a80d308bab9fc2
Nick Jenkins <nick_jenkins@outlook.com>
2021-11-26 17:42:38 -0500

    Add example with multiple WITH and stratified sampling with random

--- schemas/queries/window_lag.sql
+++ schemas/queries/window_lag.sql
@@ -1,6 +1,7 @@
 -- Example of window lag function.
 -- LAG can be used to offset rows.
 -- LEAD similar just other direction; kinda useless cause we can use negatives!
+-- Optional third argument to fill Null values
 -- Often the order by will match to aid inspection that it's working
 -- The Partition in this case is used to assist the offset by applying
 -- Fare difference according to whether passenger survived or not
@@ -9,7 +10,7 @@ SELECT
     age,
     survived,
     fare,
-    LAG(fare, 1) OVER (
+    LAG(fare, 1, 0) OVER (
         PARTITION BY survived
         ORDER BY age, name
     ) AS fare_change
